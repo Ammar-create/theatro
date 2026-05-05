a200ea9c0944838d223901928c73b0e36bd73879
Ammar-create <lokiodinson399@gmail.com>
2026-05-05 23:58:07 +0500

    feat: re-export providerStore and settingsStore in stores index

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,20 @@
 import { characterStore } from './CharacterStore.js';
 import { scenarioStore } from './ScenarioStore.js';
+import { providerStore } from './ProviderStore.js';
+import { settingsStore } from './SettingsStore.js';
 
 export { characterStore } from './CharacterStore.js';
 export { scenarioStore } from './ScenarioStore.js';
 export { chatStore } from './ChatStore.js';
 export { appEvents, appState } from './appState.js';
+export { providerStore } from './ProviderStore.js';
+export { settingsStore } from './SettingsStore.js';
 
 export async function initializeStores(): Promise<void> {
-  await characterStore.load();
-  await scenarioStore.load();
+  await Promise.all([
+    characterStore.load(),
+    scenarioStore.load(),
+    providerStore.load(),
+    settingsStore.load(),
+  ]);
 }
